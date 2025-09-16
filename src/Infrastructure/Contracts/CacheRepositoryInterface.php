b0ac32ff47111c03592987e14029cb303cf6586f
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-16 18:14:14 -0300

    feat: adiciona contrato para uso do Repository

---
+++ src/Infrastructure/Contracts/CacheRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Infrastructure\Contracts;
+
+
+interface CacheRepositoryInterface {
+    public function get(string $key): mixed;
+    public function set(string $key, mixed $data): bool;
+}
