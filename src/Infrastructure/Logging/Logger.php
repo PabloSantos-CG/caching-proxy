2e5f48bb88ea11fd6b7e6a9224eee3e91208b479
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-22 18:11:10 -0300

    feat: adiciona classe de log

---
+++ src/Infrastructure/Logging/Logger.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Infrastructure\Logging;
+
+use App\Utils\CLIManager;
+use App\Utils\DatetimeManager;
+
+class Logger
+{
+    private string $path;
+
+    public function __construct()
+    {
+        $this->path = $_SERVER['DOCUMENT_ROOT'] . '/logs';
+    }
+
+    public function writeTrace(Level $flag)
+    {
+        $data = [];
+
+        $data[] = '[' . DatetimeManager::now() . '] ';
+        $data[] = CLIManager::str() . \PHP_EOL;
+        $data[] = \debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS);
+        $data[] = '[REQUEST_URI::' . $_SERVER['REQUEST_URI'] . '] ';
+        $data[] = "$flag;" . \PHP_EOL;
+
+        return \file_put_contents($this->path, $data, \FILE_APPEND);
+    }
+}
