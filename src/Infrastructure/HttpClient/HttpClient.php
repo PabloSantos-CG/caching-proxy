8a83a859bd1d04e3a1488d0991d46e7ee3ca6263
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-23 18:03:38 -0300

    refactor: remove código não utilizado

--- src/Infrastructure/HttpClient/HttpClient.php
+++ src/Infrastructure/HttpClient/HttpClient.php
@@ -3,18 +3,10 @@
 namespace App\Infrastructure\HttpClient;
 
 use App\Infrastructure\Contracts\HttpClientInterface;
-use App\Utils\CLIManager;
 use Exception;
 
 class HttpClient implements HttpClientInterface
 {
-    private string $url;
-
-    public function __construct()
-    {
-        $this->url = CLIManager::run()['url'];
-    }
-
     /**
      * @param string[] $headers 
      * 
@@ -22,11 +14,11 @@ class HttpClient implements HttpClientInterface
      * 
      * @throws \Exception
      */
-    public function get(mixed $headers = []): mixed
+    public static function get(string $url, mixed $headers = []): mixed
     {
         $cURL = \curl_init();
 
-        \curl_setopt($cURL, \CURLOPT_URL, $this->url);
+        \curl_setopt($cURL, \CURLOPT_URL, $url);
         \curl_setopt($cURL, CURLOPT_RETURNTRANSFER, 1);
         \curl_setopt($cURL, \CURLOPT_HTTPHEADER, $headers);
 
