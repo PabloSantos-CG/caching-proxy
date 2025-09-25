9d6cc8a62299d9018767ea92e570b46454f0d5f8
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-25 17:16:35 -0300

    refactor: permite adicionar mensagem

--- src/Infrastructure/Logging/Logger.php
+++ src/Infrastructure/Logging/Logger.php
@@ -2,27 +2,32 @@
 
 namespace App\Infrastructure\Logging;
 
+use App\Infrastructure\Contracts\LoggerInterface;
 use App\Utils\DatetimeManager;
 
-class Logger
+class Logger implements LoggerInterface
 {
     private string $path;
+    private ?string $message;
 
-    public function __construct()
+    public function __construct(?string $message = null)
     {
         $this->path = $_SERVER['DOCUMENT_ROOT'] . '/logs';
+        $this->message = $message ?? $this->message;
     }
 
-    public function writeTrace(Level $flag)
+    public function writeTrace(Level $flag): bool
     {
         $data = [];
 
         $data[] = '[' . DatetimeManager::now() . '] ';
+        $data[] = "$flag" . \PHP_EOL;
         $data[] = $_ENV['HOST_PORT'] . ':' . $_ENV['ORIGIN'] . \PHP_EOL;
         $data[] = \debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS);
-        $data[] = '[URI::/' . $_GET['url'] . '] ';
-        $data[] = "$flag;" . \PHP_EOL;
+        $data[] = '[URI::/' . $_GET['url'] . ']';
+        $data[] = $this->message ? \PHP_EOL . "$this->message;" : ";";
+        $data[] = \PHP_EOL;
 
-        return \file_put_contents($this->path, $data, \FILE_APPEND);
+        return (bool) \file_put_contents($this->path, $data, \FILE_APPEND);
     }
 }
