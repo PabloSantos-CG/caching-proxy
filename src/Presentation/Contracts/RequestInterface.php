db994228f4b8b5985e77ae2f36925a61f3d9c622
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-28 10:31:41 -0300

    refactor: adiciona valor padrão ao parâmetro

--- src/Presentation/Contracts/RequestInterface.php
+++ src/Presentation/Contracts/RequestInterface.php
@@ -3,7 +3,7 @@
 namespace App\Presentation\Contracts;
 
 interface RequestInterface {
-    public function header(?string $key): mixed;
+    public function header(?string $key = null): mixed;
     public function method(): string;
     public function getQueryStr(?string $key): mixed;
     public function getUrl(): string;
