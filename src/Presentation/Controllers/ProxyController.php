a2906ac7a7d230c39af59a0e33d06a08113d2bcd
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-28 10:31:04 -0300

    feat: adiciona controller

---
+++ src/Presentation/Controllers/ProxyController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Presentation\Controllers;
+
+use App\Application\Contracts\ProxyServiceInterface;
+use App\Application\Services\ProxyService;
+use App\Presentation\Contracts\ProxyControllerInterface;
+use App\Presentation\Contracts\RequestInterface;
+use App\Presentation\Contracts\ResponseInterface;
+
+class ProxyController implements ProxyControllerInterface
+{
+    private ProxyServiceInterface $proxyService;
+
+    public function __construct()
+    {
+        $this->proxyService = new ProxyService();
+    }
+
+    public function index(
+        RequestInterface $request,
+        ResponseInterface $response
+    ): void {
+        try {
+            /**
+             * @var array{
+             *     headers: array<string, string>,
+             *     body: string,
+             *     last_modified: \DateTimeInterface,
+             *     "url:rate_limit": int
+             * }
+             * 
+             * Body is JSON encoded
+             */
+            $data = $this->proxyService->index(
+                $request->getUrl(),
+                $request->header()
+            );
+
+            $body = \json_decode($data['body']);
+
+            $response->json(
+                $data['headers'],
+                $body
+            );
+        } catch (\Throwable $th) {
+            $response->json(
+                data: [
+                    'status' => 'error',
+                    'message' => $th->getMessage()
+                ],
+                status_code: $th->getCode()
+            );
+        }
+    }
+}
