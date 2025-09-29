3aa495d68cf4eb40c3660a2a0401bdf0aadeabda
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-29 18:02:04 -0300

    feat: adiciona maneira de apagar dados do redis via CLI

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-App\Utils\RequestFilter::filter();
-
-use App\Core\Core;
 use Dotenv\Dotenv;
+use Predis\Client as PredisClient;
+use App\Core\Core;
 
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+if (php_sapi_name() === 'cli' && in_array('--clear', $_SERVER['argv'])) {
+    $predisClient = new PredisClient([
+        'host' => '127.0.0.1',
+        'port' => $_ENV['REDIS_PORT'],
+        'password' => $_ENV['REDIS_PASSWORD'],
+    ]);
+
+    $predisClient->flushall();
+    exit();
+}
+
+App\Utils\RequestFilter::filter();
 
 $core = new Core();
 $core->run();
